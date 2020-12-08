d39236f3d76d14e6a76421f67ccaae0efd236910
shiseki toshikazu <uw-t.a.k.n.s-10ve@ezweb.ne.jp>
2020-12-08 14:11:22 +0000

    [Add] 投稿機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'todolists/new'
-	get 'top' => 'homes#top'
+  get 'top' => 'homes#top'
+  post 'todolists' => 'todolists#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
