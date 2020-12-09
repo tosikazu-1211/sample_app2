be28269417b62eaa91a6a98dae11b3ba09e6e7ae
shiseki toshikazu <uw-t.a.k.n.s-10ve@ezweb.ne.jp>
2020-12-09 11:15:48 +0000

    [Add] 閲覧機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'todolists/new'
   get 'top' => 'homes#top'
   post 'todolists' => 'todolists#create'
+  get 'todolists' => 'todolists#indes'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
