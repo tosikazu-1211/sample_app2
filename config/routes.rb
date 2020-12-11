d73a6313a6436df068758811d88353793fb9d01b
shiseki toshikazu <uw-t.a.k.n.s-10ve@ezweb.ne.jp>
2020-12-11 11:26:44 +0000

    [Add] view showページの追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'top' => 'homes#top'
   post 'todolists' => 'todolists#create'
   get 'todolists' => 'todolists#indes'
-  get 'todolists/:id' => 'todolists#show', as: 'todolists'
+  get 'todolists/:id' => 'todolists#show', as: 'todolist'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
