f52e2eadc6f1e1c8251d2ff8f2ade5a6f6f1086d
shiseki toshikazu <uw-t.a.k.n.s-10ve@ezweb.ne.jp>
2020-12-11 11:22:43 +0000

    [Update] ルーティングの更新

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'top' => 'homes#top'
   post 'todolists' => 'todolists#create'
   get 'todolists' => 'todolists#indes'
+  get 'todolists/:id' => 'todolists#show', as: 'todolists'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
