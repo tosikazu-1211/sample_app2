9a3170f6abe9d065d9caab00387460a9528e3aef
shiseki toshikazu <uw-t.a.k.n.s-10ve@ezweb.ne.jp>
2020-12-14 14:22:58 +0000

    [Add] 画像投稿機能の追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '115d46e9c8398b6d89146220e3c92fd8be1d9bc34932bf464fbab418e5ab6950c49c183caa0adf12d6a3c9e45573474502b2b0e8c4e391998df41e51f9259858'
+
